46fb1d70e3ebaf45093a9a5688e2ed836a8bef30
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-19 10:51:38 +1000

    create constant class to store constants

---
+++ Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace DevSpot.Constants
+{
+    public static class Roles
+    {
+        public const string ADMIN = "admin";
+        public const string JOB_SEEKER = "JobSeeker";
+        public const string EMPLOYER = "Employer";
+    }
+}
