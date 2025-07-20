0fa5aef3a893d04719c6f137d9bd4fe17b687c86
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-20 15:06:31 +1000

    create first unit test

---
+++ DevSpot.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using DevSpot.Data;
+using DevSpot.Models;
+using DevSpot.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DevSpot.Tests
+{
+    public class JobPostingRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+        public JobPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("JobPostingDb")
+                .Options;
+        }
+        private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+
+        [Fact]
+        public async Task AddAsync_ShouldAddJobPosting()
+        {
+            var db = CreateDbContext();
+            var repository = new JobPostingRepository(db);
+            var jobPosting = new JobPosting
+            {
+                Title = "Test",
+                Description = "Test Dec",
+                PostedDate = DateTime.Now,
+                Company = "Test Company",
+                Location = "Test Location",
+                UserId = "Test userId"
+            };
+            await repository.AddAsync(jobPosting);
+            var result = db.JobPostings.SingleOrDefault(x => x.Title == "Test");
+            Assert.NotNull(result);
+            Assert.Equal("Test Dec", result.Description);
+        }
+    }
+}
