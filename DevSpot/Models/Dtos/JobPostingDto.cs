2390c858a794ec99599e64f3ed59db4412bdc60d
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-23 10:44:30 +1000

    change LoginUser to LoginUserDto and create JobPostingDto

---
+++ DevSpot/Models/Dtos/JobPostingDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpot.Models.Dtos
+{
+    public class JobPostingDto
+    {
+        [Required]
+        public required string Title { get; set; }
+        [Required]
+        public required string Description { get; set; }
+        [Required]
+        public required string Company { get; set; }
+        [Required]
+        public required string Location { get; set; }
+    }
+}
